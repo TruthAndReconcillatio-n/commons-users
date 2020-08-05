fa95ced853856ad30faa5d0cbd230fdafeff65b6
karunsane <karunsane@gmail.com>
2020-08-05 12:52:44 -0500

    Init

---
+++ src/main/java/com/fca/dev/springboot/app/users/commons/SpringBootServiceCommonsUsersApplication.java
@@ -0,0 +1,10 @@
+package com.fca.dev.springboot.app.users.commons;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class SpringBootServiceCommonsUsersApplication {
+}
---
+++ src/test/java/com/fca/dev/springboot/app/users/commons/SpringBootServiceCommonsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fca.dev.springboot.app.users.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServiceCommonsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
